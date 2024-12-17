4c6237fa6eb6c247025b284097e7388bb746ee0b
React-Crafter <abulbasar1725@gmail.com>
2024-12-17 10:42:41 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Nopmo from './components/Nopmo/Nopmo'
+function App() {
+
+    return (
+        <div>
+            <Nopmo></Nopmo>
+        </div>
+    )
+}
+
+export default App
